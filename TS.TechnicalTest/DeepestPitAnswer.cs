8a3a1e076a6c17888899ffe9d6d07534440d7dcc
Brian Phiri <brnphiri@gmail.com>
2026-02-04 06:44:05 +0200

    start counting only above the ground

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -5,5 +5,11 @@ public static class DeepestPitAnswer
     public static int Solution(int[] points)
     {
         if (points is { Length : < 3 }) return -1;
+
+        for (var p = 0;  p < points.Length - 2 ; p ++)
+        {
+           if(points[p] <= 0) continue; 
+             
+        }
     }
 }
