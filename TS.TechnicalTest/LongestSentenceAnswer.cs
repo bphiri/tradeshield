5e7d9deab69ce6aec8d28ea51209565b1d32b4d2
Brian Phiri <brnphiri@gmail.com>
2026-02-04 06:12:29 +0200

    split each sentence into list of words for counting, use collection expressions

--- TS.TechnicalTest/LongestSentenceAnswer.cs
+++ TS.TechnicalTest/LongestSentenceAnswer.cs
@@ -6,7 +6,13 @@ public static class LongestSentenceAnswer
     public static int Solution(string s)
     {
         if (string.IsNullOrWhiteSpace(s)) return 0;
-        var questions = s.Split( new char[] { '.' , '?', '!' , }, StringSplitOptions.RemoveEmptyEntries);
+        var questions = s.Split(['.' , '?', '!'], StringSplitOptions.RemoveEmptyEntries);
+        var maximumWords = 0;
+        
+        foreach (var question in questions)
+        {
+            var words = s.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+        }
         
         
     }
