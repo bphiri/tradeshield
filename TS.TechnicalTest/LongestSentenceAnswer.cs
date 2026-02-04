1f0152222b47832837b777ca776b0177bb818736
Brian Phiri <brnphiri@gmail.com>
2026-02-04 06:09:06 +0200

    split string into a list of questions

--- TS.TechnicalTest/LongestSentenceAnswer.cs
+++ TS.TechnicalTest/LongestSentenceAnswer.cs
@@ -6,6 +6,7 @@ public static class LongestSentenceAnswer
     public static int Solution(string s)
     {
         if (string.IsNullOrWhiteSpace(s)) return 0;
+        var questions = s.Split( new char[] { '.' , '?', '!' , }, StringSplitOptions.RemoveEmptyEntries);
         
         
     }
